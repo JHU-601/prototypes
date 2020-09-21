f7fd1ebebdd0b758fb007f163c6cc1c348edfd2d
Stephen Grice <stephengrice@live.com>
2020-09-21 13:24:31 -0400

    Get bidirectional websockets comms working
    on page /gameroom

--- flask-prototype/server.py
+++ flask-prototype/server.py
@@ -1,15 +1,30 @@
 # Clueless Flask Prototype
 from flask import Flask
 from flask import render_template
+from flask_socketio import SocketIO, emit
 
 SERVER_ADDR = '0.0.0.0'
 SERVER_PORT = 5000
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/gameroom')
+def gameroom():
+    return render_template('gameroom.html')
+
+@socketio.on('connect')
+def handle_connection():
+    print('Client connected')
+    emit('my event', {'data': 'hello there young connection'})
+    print('sent welcome message')
+@socketio.on('my event')
+def handle_my_event(json):
+    print("From client:",json['data'])
+
 if __name__ == '__main__':
-    app.run(host=SERVER_ADDR, port=SERVER_PORT)
+    socketio.run(app, host=SERVER_ADDR, port=SERVER_PORT, debug=True)
--- flask-prototype/static/js/main.js
+++ flask-prototype/static/js/main.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+function initGameroom() {
+  var socket = io.connect();
+  socket.on('connect', function() {
+    console.log('connected')
+    socket.emit('my event', {data: 'iron board games are the best'});
+  });
+  socket.on('my event', function(json) {
+    console.log('from server: ' + json.data);
+  })
+}
+
+$(document).ready(function() {
+  if (window.location.pathname.includes('gameroom')) {
+    initGameroom();
+  }
+});
